8b0b29ba2010965ae2816691b4125fd29cc5b848
Sebb <sebb@apache.org>
2018-05-02 12:59:53 +0100

    Empty

--- README.md
+++
@@ -1,5 +0,0 @@
-This is a test repo for Apache Attic
-
-The contents are liable to change or disappear at any time
-
---ENDS--
--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Dummy build script
-
-OUT=${1:?}
-test -d ${OUT} || mkdir ${OUT}
-date >${OUT}/date.txt
